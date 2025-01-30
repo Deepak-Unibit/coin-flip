5fd114b344cc3dceebdd977ec31bc94337af5831
Deepak <deepak.unibit@gmail.com>
2025-01-30 11:42:20 +0530

    fgfgd

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
+import 'dart:io';
 import 'package:flip_coin/utils/theme.util.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import 'api/call.api.dart';
 import 'modules/home/view/home.view.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  HttpOverrides.global = MyHttpOverrides();
+  ApiCall.configureDio();
   runApp(const MyApp());
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -16,6 +27,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
       theme: ThemesUtil.light,
       home: HomeView(),
     );
